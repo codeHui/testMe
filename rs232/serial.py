5e5cfd3a6deeffe4cec9b09680c0b0e34a3a9385
codeHui <hao_chun_hui@163.com>
2024-03-13 14:36:37 +0800

    Update serial.py

--- rs232/serial.py
+++ rs232/serial.py
@@ -1,7 +1,7 @@
 import serial  
   
-ser = serial.Serial('/dev/serial0', 9600)  # 使用9600波特率打开串行端口  
+ser = serial.Serial('/dev/serial0', 9600) 
   
 while True:  
-    data = ser.readline()  # 读取一行数据  
-    print data  # 打印收到的数据  
+    data = ser.readline() 
+    print data 
